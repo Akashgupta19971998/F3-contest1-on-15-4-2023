420e5940d57300d8af6c3569bd676e8ea41f319a
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-01-14 18:07:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const fname = document.getElementById("fname");
+const profession = document.getElementById("profession");
+const age = document.getElementById("age");
+const message = document.getElementById("message");
+const tbody = document.getElementById("tbody");
+const button=document.getElementById('button')
+
+const arr=[];
+
+button.addEventListener("click", success)
+
+function success() {
+     alert("hello")
+    if (!(fname.value && profession.value && age.value)) {
+      message.innerText = "Error";
+      message.style.display = "block";
+      message.style.color = "red";
+      return;
+    }
+    message.innerText = "Success:Employee Added";
+    message.style.display = "block";
+    message.style.color = "green";
+    arr.push({
+      fname:fname.value,
+      profession:profession.value,
+      age:age.value,
+    });
+     console.log(arr);
+    renderData();
+  }
+
+  function renderData() {
+    arr.forEach((e) => {
+      const row = `
+      <tr>
+        <td>${e.name}</td>
+        <td>${e.profession}</td>
+        <td>${e.age}</td>
+      </tr>
+    `;
+
+      tbody.appendChild(row);
+    });
+  }
