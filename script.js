260ebf139407ff4f1178ef346a25c10f8f3c1684
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-01-14 18:11:26 +0530

    commit

--- script.js
+++ script.js
@@ -3,42 +3,45 @@ const profession = document.getElementById("profession");
 const age = document.getElementById("age");
 const message = document.getElementById("message");
 const tbody = document.getElementById("tbody");
-const button=document.getElementById('button')
+const button = document.getElementById('button')
 
-const arr=[];
+const arr = [];
+let num = 0;
 
 button.addEventListener("click", success)
 
 function success() {
-     alert("hello")
+    alert("hello")
     if (!(fname.value && profession.value && age.value)) {
-      message.innerText = "Error";
-      message.style.display = "block";
-      message.style.color = "red";
-      return;
+        message.innerText = "Error";
+        message.style.display = "block";
+        message.style.color = "red";
+        return;
     }
     message.innerText = "Success:Employee Added";
     message.style.display = "block";
     message.style.color = "green";
     arr.push({
-      fname:fname.value,
-      profession:profession.value,
-      age:age.value,
+        num: num++,
+        fname: fname.value,
+        profession: profession.value,
+        age: age.value,
     });
-     console.log(arr);
+    console.log(arr);
     renderData();
-  }
+}
 
-  function renderData() {
+function renderData() {
     arr.forEach((e) => {
-      const row = `
+        const row = `
       <tr>
         <td>${e.name}</td>
         <td>${e.profession}</td>
         <td>${e.age}</td>
+        <td><td>
       </tr>
     `;
 
-      tbody.appendChild(row);
+        tbody.appendChild(row);
     });
-  }
+}
