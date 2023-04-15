883a5d6b5e61c61ff42d68c8e4fd3021469314aa
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-04-15 19:22:33 +0530

    F3-contest-1(15-04-2023)

--- script.js
+++ script.js
@@ -5,23 +5,30 @@ const message = document.getElementById("message");
 const tbody = document.getElementById("tbody");
 const btn11 = document.getElementById('button1')
 
-const arr = [];
+let arr = [];
 let num = 1;
 
 btn11.addEventListener("click", success);
 
 function success() {
     if (!(fname.value && profession.value && age.value)) {
-        message.innerText = "Error";
+        message.innerText = "Error : Please make sure all the fields are filled before adding in an employee!";
         message.style.display = "block";
         message.style.color = "red";
         return;
     }
-    message.innerText = "Success:Employee Added";
+    document.getElementById("one").style.display = "none";
+    document.getElementById("zero").style.display = "none";
+    message.innerText = "Success : Employee Added!";
     message.style.display = "block";
     message.style.color = "green";
+    document.getElementById("two").style.display = "block";
+    // let new1=document.createElement("h3");
+    // new1.innerHTML="Added Employees"
+    // new1.style.display = "block";
+    // console.log(new1)
     arr.push({
-        num: num++,
+        num: num++, //id:1 id:2
         fname: fname.value,
         profession: profession.value,
         age: age.value,
@@ -29,23 +36,34 @@ function success() {
     console.log(arr);
     renderData();
 }
-
+// var table = document.createElement("table");
 function renderData() {
     arr.forEach((e) => {
         const row = `
+        <tbody>
       <tr>
-        <td>${e.num}<td>
-        <td>${e.name}</td>
-        <td>${e.profession}</td>
-        <td>${e.age}</td>
-        <td><button onclick="Delete_user('${e.num}')">${Delete_user}</button><td>
+        <td>${"Id: " + e.num}<td>
+        <td>${"Name: " + e.fname}</td>
+        <td>${"Profession: " + e.profession}</td>
+        <td>${"Age: " + e.age}</td>
+     <td><button onclick="Delete_user('${e.num}')">${Delete_user}</button></td>
       </tr>
+      </tbody>
     `;
-
-        tbody.appendChild(row);
+         tableBody.appendChild(row);
+        // table.appendChild(row);
+        console.log("row>> " + row)
     });
 }
+var tableBody = document.getElementById("table");
 
-function Delete_user(i){
+var tableRows = arr.map(function (person) {
+    return "<tr><td> "+num+" </td><td>" + person.name + "</td><td>" + person.age + "</td><td>" + person.city + "</td></tr>";
+});
+
+ tableBody.innerText = tableRows.join("");
+
+
+function Delete_user(i) {
 
 }
